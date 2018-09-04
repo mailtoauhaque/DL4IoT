45ad090eefd313fef3dc5948f9d7e3242d9ce39b
DPDMPROJECT <anwar@codexnow.com>
2018-09-04 12:37:49 +0500

    Commit 2

--- GitTestCase.py
+++ GitTestCase.py
@@ -1,1 +1,20 @@
-print ('Test Case')
+#Github : mailtoauhaque
+#Python interpreter: 3.6
+#Deep Learning 4 IoT Course
+
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print (train_images.shape)
